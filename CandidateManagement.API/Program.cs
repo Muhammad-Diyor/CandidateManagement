1470cade68deb24f8613af6e571d63a8f345b828
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2025-03-26 05:29:12 +0900

    Implemented caching using Redis

--- CandidateManagement.API/Program.cs
+++ CandidateManagement.API/Program.cs
@@ -1,18 +1,28 @@
 using CandidateManagement.API.Middleware;
 using CandidateManagement.API.Validators;
+using CandidateManagement.Application.Caching;
 using CandidateManagement.Application.Repositories;
 using CandidateManagement.Application.Services;
+using CandidateManagement.Infrastructure.Caching;
 using CandidateManagement.Infrastructure.Data;
 using CandidateManagement.Infrastructure.Repositories;
 using CandidateManagement.Infrastructure.Services;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var configuration = ConfigurationOptions.Parse("localhost:6379", true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
@@ -20,13 +30,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddValidatorsFromAssemblyContaining<CandidateDtoValidator>();
 builder.Services.AddFluentValidationAutoValidation();
 
+builder.Services.AddScoped<ICacheService, RedisCacheService>();
 builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
 builder.Services.AddScoped<ICandidateService, CandidateService>();
 
-
-
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
